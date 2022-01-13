c009ac578b245fe1b424c53f4fd61b0fd2a02910
David Schüler <dav.schueler@arcor.de>
2022-01-14 00:07:27 +0100

    first

--- backend/app/app/tester.py
+++
@@ -1,7 +0,0 @@
-import requests
-from time import sleep
-
-BASE = "http://localhost/api/"
-#requests.put(BASE + "spiked/test/", {"base": 8, "distance": 2,"propability":.6, "size":4, "transmissionFrequency":1})
-#sleep(2)
-requests.put(BASE + "random/test/", {"lowerBoundary": 5, "upperBoundary": 90, "transmissionFrequency":1})
