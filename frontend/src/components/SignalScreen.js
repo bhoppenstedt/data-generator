a665fd50e0f4f1141ce9bddf6fc7e39bd618540c
Florian Hauss <florian.hauss@uni-ulm.de>
2022-01-20 23:39:48 +0100

    added currentSignalType variable

--- frontend/src/components/SignalScreen.js
+++ frontend/src/components/SignalScreen.js
@@ -37,6 +37,8 @@ const SignalScreen = () => {
         "test1","test2","test3","test4","test5","test6"
     ]
 
+    var currentSignalType
+
     function putReq(streamType, streamName, params) {
         
           fetch('/api/'+ streamType + '/' + streamName + '/', {
@@ -69,6 +71,12 @@ const SignalScreen = () => {
           .then(json => console.log(json));
 
     }
+    
+    function changeCurrentSignal(type){
+        console.log(type)
+        currentSignalType = type
+        console.log(currentSignalType)
+    }
 
     return (
         <Card variant= "outlined" sx={{bgcolor: 'background.paper', border: 3, borderColor: '#A1A5F9',height:"85vh", marginLeft: "8vw", marginRight: "1vw", boxShadow: '3px 3px 10px 1px rgba(0, 0, 0, 0.16)'}}>
@@ -79,15 +87,15 @@ const SignalScreen = () => {
                             signal type
                         </Typography>
 
-                        <SignalButton name={"Random signal"} onClick={() => setConfig(setShowRS)} icon={<CasinoOutlinedIcon sx={{ fontSize: 20, fontFamily: '"Roboto", sans-serif'}} />}/>
+                        <SignalButton name={"Random signal"} onClick={() => setConfig(setShowRS), changeCurrentSignal("random")}  icon={<CasinoOutlinedIcon sx={{ fontSize: 20, fontFamily: '"Roboto", sans-serif'}} />}/>
 
-                        <SignalButton name={"Sinus signal"} onClick={() => setConfig(setShowSS)} icon={<CasinoOutlinedIcon sx={{ fontSize: 50}} />}/>
+                        <SignalButton name={"Sinus signal"} onClick={() => setConfig(setShowSS), changeCurrentSignal("sinus")}  icon={<CasinoOutlinedIcon sx={{ fontSize: 50}} />}/>
 
-                        <SignalButton name={"Cosinus signal"} onClick={() => setConfig(setShowCS)} icon={<AutoGraphOutlinedIcon sx={{ fontSize: 50}}/>}/>
+                        <SignalButton name={"Cosinus signal"} onClick={() => setConfig(setShowCS), changeCurrentSignal("cosinus")}  icon={<AutoGraphOutlinedIcon sx={{ fontSize: 50}}/>}/>
 
-                        <SignalButton name={"Spiked signal"} onClick={() => setConfig(setShowSWS)} icon={<AutoGraphOutlinedIcon sx={{ fontSize: 50}} />}/>
+                        <SignalButton name={"Spiked signal"} onClick={() => setConfig(setShowSWS), changeCurrentSignal("spiked")}  icon={<AutoGraphOutlinedIcon sx={{ fontSize: 50}} />}/>
 
-                        <SignalButton name={"Normally distributed signal"} onClick={() => setConfig(setShowNS)} icon={<TimelineOutlinedIcon sx={{ fontSize: 30}} />}/>
+                        <SignalButton name={"Normally distributed signal"} onClick={() => setConfig(setShowNS), changeCurrentSignal("emphasized")}  icon={<TimelineOutlinedIcon sx={{ fontSize: 30}} />}/>
 
                     </Stack>
                 </Grid>
@@ -105,7 +113,7 @@ const SignalScreen = () => {
                                     : showsws ? <SpikesSignal handleChange={(e) => {}} numberformat={value.numberformat}/>
                                         : showns ? <NormallyDistributed handleChange={(e) => {}} numberformat={value.numberformat}/>
                                             : null }
-                        <SignalButton name={"Generate"} onClick={() => putReq()} icon={<></>}/>
+                        <SignalButton name={"Generate"} onClick={() => putReq(currentSignalType,"kevin",{lowerBoundary:1, upperBoundary:10, transmissionFrequency:1})} icon={<></>}/>
                     </Stack>
                 </Grid>
             </Grid>
