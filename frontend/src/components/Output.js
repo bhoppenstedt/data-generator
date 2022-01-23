7b1fc2d03cc49dc1728788f54e07ade0d00c0340
David Schüler <dav.schueler@arcor.de>
2022-01-23 20:34:19 +0100

    streams now show accordingly

--- frontend/src/components/Output.js
+++ frontend/src/components/Output.js
@@ -1,5 +1,6 @@
 import Card from "@mui/material/Card";
 import Paper from "@mui/material/Paper";
+
 import Typography from "@mui/material/Typography";
 import {purple} from "@mui/material/colors";
 import Grid from "@mui/material/Grid";
@@ -10,65 +11,9 @@ import Stack from "@mui/material/Stack";
 import React, { useEffect, useState } from "react";
 import StreamBoxElem from "./StreamBoxElem.js"
 
+var array = [];
+const streamsServer = JSON.stringify(fetch('/api/signals/').then(res => res.json()).then(dataJSON => JSON.parse(dataJSON)).then(data => array = Array.from(data)).then(() => console.log(array)))
 
-const streams1= [
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-    {
-        "lowerBoundary": 2,
-        "upperBoundary": 55,
-        "transmissionFrequency": 6.0,
-        "type": "random",
-        "running": false,
-        "name": "test1"
-    },
-]
 
 const streams2= [
     {
@@ -79,6 +24,55 @@ const streams2= [
         "running": true,
         "name": "test1"
     },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+    
 ]
 
 const Output = () => {
@@ -89,7 +83,7 @@ const Output = () => {
     <StreamBoxElem name={stream.name} type={stream.type} argument1={stream.lowerBoundary} argument2={stream.upperBoundary} argument3={stream.transmissionFrequency} runningState={stream.running}/>);
 
     return (
-            <Paper variant="outlined" sx={{ bgcolor: 'background.paper', border: 3, borderColor: '#A1A5F9', marginLeft: "1vw", marginRight: "8vw", height: "85vh", boxShadow: '3px 3px 10px 1px rgba(0, 0, 0, 0.16)'}}>
+            <Card variant="outlined" sx={{bgcolor: 'background.paper', border: 3, borderColor: '#A1A5F9', marginLeft: "1vw", marginRight: "8vw", height: "85vh", boxShadow: '3px 3px 10px 1px rgba(0, 0, 0, 0.16)'}}>
                 <Grid container direction="column" wrap="nowrap" justifyContent="center" alignItems="center">
                     <Grid item xs={12}>
                         <Typography component="div" sx={{ fontSize: 25, fontWeight: "bold", color: '#3F0092', marginTop:"0.1vw", marginBottom:"0.5vw" }}>
@@ -98,7 +92,7 @@ const Output = () => {
                     </Grid>
                     
                     <Grid item xs={12} sx={{width: "100%"}}>
-                        <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
+                        <Grid height="100%" container direction="row" justifyContent="space-evenly" alignItems="center">
                             <Grid item xs={6} justifyContent="left" alignItems="left">
                                 <Typography component="div" variant="h5" sx={{ fontSize: 20,fontFamily: 'Open Sans, sans-serif', fontWeight: "600",fontSize: 20, color: purple[900], margin: "10px" }} align={"left"}>
                                     active streams:
@@ -109,7 +103,7 @@ const Output = () => {
                                         <Typography component="div" variant="h5" sx={{ fontSize: 20,fontFamily: 'Open Sans, sans-serif', fontWeight: "600",fontSize: 20, color: purple[900]}} align={"right"}>
                                             start all
                                         </Typography>
-                                        <IconButton onClick={ () => setStreams(streams2)}>
+                                        <IconButton onClick={ () => setStreams(array)}>
                                             <PlayCircleOutlineOutlinedIcon
                                                 sx={{ fontSize: 30, color: purple[900] }}
                                             />
@@ -118,7 +112,7 @@ const Output = () => {
                                         <Typography component="div" variant="h5" sx={{ fontSize: 20,fontFamily: 'Open Sans, sans-serif', fontWeight: "600",fontSize: 20, color: purple[900]}} align={"right"}>
                                             stop all
                                         </Typography>
-                                        <IconButton onClick={ () => setStreams(streams1)}>
+                                        <IconButton>
                                             <StopCircleOutlinedIcon
                                                 sx={{ fontSize: 30, color:purple[900] }}
                                             />
@@ -127,14 +121,14 @@ const Output = () => {
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} sx={{width: "95%"}}>
-                        <Paper sx={{ overflowY:"auto"}}>
-                            {streamElements}
-                        </Paper>
+                    <Grid item xs={12} sx={{width: "96%", overflowY:"scroll"}}>
+    
+                        {streamElements}
+
                     </Grid>
 
                 </Grid>
-            </Paper>
+            </Card>
 
     )
 }
