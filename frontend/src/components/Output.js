423009e7893eda3dbced90c4a3a98967856bfdf5
David Schüler <dav.schueler@arcor.de>
2022-01-23 15:43:36 +0100

    able to read data from streams

--- frontend/src/components/Output.js
+++ frontend/src/components/Output.js
@@ -1,4 +1,5 @@
 import Card from "@mui/material/Card";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import {purple} from "@mui/material/colors";
 import Grid from "@mui/material/Grid";
@@ -6,28 +7,89 @@ import {IconButton} from "@mui/material";
 import PlayCircleOutlineOutlinedIcon from "@mui/icons-material/PlayCircleOutlineOutlined";
 import StopCircleOutlinedIcon from "@mui/icons-material/StopCircleOutlined";
 import Stack from "@mui/material/Stack";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import StreamBoxElem from "./StreamBoxElem.js"
+
+
+const streams1= [
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": false,
+        "name": "test1"
+    },
+]
+
+const streams2= [
+    {
+        "lowerBoundary": 2,
+        "upperBoundary": 55,
+        "transmissionFrequency": 6.0,
+        "type": "random",
+        "running": true,
+        "name": "test1"
+    },
+]
 
 const Output = () => {
-
-    useEffect(() => {
-        fetch('/api/signals/')
-        //fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(res => {
-            console.log(res);
-            return res.json();
-        })
-        .then(data => {
-            //const obj = JSON.parse(data);
-            //console.log(obj);
-            //console.log(Object.values(obj));
-            
-        })
-    }, []);
     
+    const [streams, setStreams] = useState(streams2);
+
+    const streamElements = streams.map((stream) => 
+    <StreamBoxElem name={stream.name} type={stream.type} argument1={stream.lowerBoundary} argument2={stream.upperBoundary} argument3={stream.transmissionFrequency} runningState={stream.running}/>);
 
     return (
-            <Card variant="outlined" sx={{bgcolor: 'background.paper', border: 3, borderColor: '#A1A5F9', marginLeft: "1vw", marginRight: "8vw", height: "85vh", boxShadow: '3px 3px 10px 1px rgba(0, 0, 0, 0.16)'}}>
+            <Paper variant="outlined" sx={{ bgcolor: 'background.paper', border: 3, borderColor: '#A1A5F9', marginLeft: "1vw", marginRight: "8vw", height: "85vh", boxShadow: '3px 3px 10px 1px rgba(0, 0, 0, 0.16)'}}>
                 <Grid container direction="column" wrap="nowrap" justifyContent="center" alignItems="center">
                     <Grid item xs={12}>
                         <Typography component="div" sx={{ fontSize: 25, fontWeight: "bold", color: '#3F0092', marginTop:"0.1vw", marginBottom:"0.5vw" }}>
@@ -38,25 +100,25 @@ const Output = () => {
                     <Grid item xs={12} sx={{width: "100%"}}>
                         <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
                             <Grid item xs={6} justifyContent="left" alignItems="left">
-                                <Typography component="div" variant="h5" sx={{ fontSize: 20, fontWeight: "bold", color: purple[900], margin: "10px" }} align={"left"}>
+                                <Typography component="div" variant="h5" sx={{ fontSize: 20,fontFamily: 'Open Sans, sans-serif', fontWeight: "600",fontSize: 20, color: purple[900], margin: "10px" }} align={"left"}>
                                     active streams:
                                 </Typography>
                             </Grid>
                             <Grid item xs={6}>
                                 <Stack direction="row" spacing={0} justifyContent="right" alignItems="center">
-                                        <Typography component="div" variant="h5" sx={{  fontSize: 20, fontWeight: "bold", color: '#3F0092', marginRight:"0px" }} align={"right"}>
+                                        <Typography component="div" variant="h5" sx={{ fontSize: 20,fontFamily: 'Open Sans, sans-serif', fontWeight: "600",fontSize: 20, color: purple[900]}} align={"right"}>
                                             start all
                                         </Typography>
-                                        <IconButton>
+                                        <IconButton onClick={ () => setStreams(streams2)}>
                                             <PlayCircleOutlineOutlinedIcon
                                                 sx={{ fontSize: 30, color: purple[900] }}
                                             />
                                         </IconButton>
                                         
-                                        <Typography component="div" variant="h5" sx={{  fontSize: 20, fontWeight: "bold", color: '#3F0092', marginRight:"0px" }} align={"right"}>
+                                        <Typography component="div" variant="h5" sx={{ fontSize: 20,fontFamily: 'Open Sans, sans-serif', fontWeight: "600",fontSize: 20, color: purple[900]}} align={"right"}>
                                             stop all
                                         </Typography>
-                                        <IconButton>
+                                        <IconButton onClick={ () => setStreams(streams1)}>
                                             <StopCircleOutlinedIcon
                                                 sx={{ fontSize: 30, color:purple[900] }}
                                             />
@@ -65,16 +127,14 @@ const Output = () => {
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} sx={{width: "100%"}}>
-                        <Card sx={{height: "85%", width: "95%", overflowY: "scroll"}} >
-                            
-                            
-
-                        </Card>
+                    <Grid item xs={12} sx={{width: "95%"}}>
+                        <Paper sx={{ overflowY:"auto"}}>
+                            {streamElements}
+                        </Paper>
                     </Grid>
 
                 </Grid>
-            </Card>
+            </Paper>
 
     )
 }
--- frontend/src/components/StreamBoxElem.js
+++ frontend/src/components/StreamBoxElem.js
@@ -12,29 +12,38 @@ import { color } from "@mui/system";
 
 
 
-const StreamBoxElem = ({name, type, format}) => {
+const StreamBoxElem = ({name, type, argument1, argument2, argument3, runningState}) => {
     return (
     <Card sx={{ height: "100px", background: 'linear-gradient( to bottom, rgba(255,255,255,1), rgba(232,232,232,1))', boxShadow: '0px 3px 6px 0px rgba(0, 0, 0, .16)', marginBottom: "10px"}}>
         <CardContent>
-            <Typography sx={{ fontSize: 20, fontWeight: "bold" }} color="rgba(1,1,1,1)" gutterBottom>
-                Datastream {name}
+            <Typography sx={{ fontFamily: "Open Sans, sans-serif", fontSize: 20, fontWeight: "600" }} color="rgba(1,1,1,1)" gutterBottom>
+                {name}
             </Typography>
 
             <Grid container item xs={12} container direction="row">
-            <Grid item xs={6}>
-                <Typography sx={{ fontSize: 15, fontWeight: "bold" }}>
-                    signal: {type}
-                </Typography>
+                <Grid item xs={4}>
+                    <Typography sx={{ fontFamily: "Open Sans, sans-serif", fontSize: 14, fontWeight: "600" }}>
+                        Type: {type}
+                    </Typography>
 
-                <Typography sx={{ fontSize: 15, fontWeight: "bold" }}>
-                    Output format: {format}
-                </Typography>
-            </Grid>
-            <Grid item xs={6} align={"right"}>
-                <Typography sx={{ fontSize: 15, color: "#5FA500", align: "left", lineHeight: "normal"}}>
-                    Running...
-                </Typography>
-            </Grid>
+                    <Typography sx={{ fontFamily: "Open Sans, sans-serif", fontSize: 14, fontWeight: "600" }}>
+                        Argument1: {argument1}
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography sx={{ fontFamily: "Open Sans, sans-serif", fontSize: 14, fontWeight: "600" }}>
+                        Argument2: {argument2}
+                    </Typography>
+
+                    <Typography sx={{ fontFamily: "Open Sans, sans-serif", fontSize: 14, fontWeight: "600" }}>
+                        Argument3: {argument3}
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} align={"right"}>
+                    <Typography sx={{ fontSize: 15, color: runningState ? "#5FA500" : "#FF0000", align: "left", lineHeight: "normal"}}>
+                        {runningState ? "Running..." : "Stopped"}
+                    </Typography>
+                </Grid>
             </Grid>
         </CardContent>
     </Card>
