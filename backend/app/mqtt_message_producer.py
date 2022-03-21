19f11599c034857c6f87c4f4ca95d3f3f407ae8f
Florian Hauss <florian.hauss@uni-ulm.de>
2022-03-21 13:59:27 +0100

    implemented reandom mqtt signal

--- backend/app/mqtt_message_producer.py
+++ backend/app/mqtt_message_producer.py
@@ -1,53 +1,71 @@
-# python 3.6
-
 import random
-import time
-
+from time import sleep 
 from paho.mqtt import client as mqtt_client
-
 
 broker = 'broker.emqx.io'
 port=1883
 topic = "python/mqtt"
 # generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+#client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
 # password = 'public'
 
-def connect_mqtt():
-    print("reached")
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
-    client.on_connect = on_connect
-    client.connect(broker,port)
-    return client
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+class Random_signal_producer(object):
+    
+    def __init__(self, name, lowerBoundary, upperBoundary, transmissionFrequency):
+        """Called when an new signal of the corresponding type is created. Creats an object that has the parameters of the signal stored in its variables. Note that the signal
+        is not intially running and has to be patched once at the start.
+
+        Args:
+            lowerBoundary (int): The lower boundary of the random signal
+            upperBoundary (int): The upper boundary of the random signal
+            transmissionFrequency(float): The pause in between ticks of the signal
+        """
+        self.client = mqtt_client.Client(name)
+        self.client.on_connect = on_connect
+
+        self.running = False
+        self.lowerBoundary = lowerBoundary
+        self.upperBoundary = upperBoundary
+        self.transmissionFrequency = transmissionFrequency
+
+    def patch(self):
+        """[Start/Pause the signal]
+
+        Returns:
+            [Bool]: [Returns true if successful. Note there's no Return when starting a signal]
+        """
+        self.running = not self.running
+        if self.running:
+            self.client.connect(broker,port)
+            self.sendRandomSignal()
+        return True
+
+    def sendRandomSignal(self):
+        """A random signal with the parameters of the corresponding signal is created and sent to the kafka topic 'Random-Signal'.
+        """
+        while(self.running):
+            random_number = int(random.randint(self.lowerBoundary,self.upperBoundary))
+            #print(f"Sending number {random_number}")
+            result = self.client.publish(topic, random_number)
+            # result: [0, 1]
+            status = result[0]
+            if status == 0:
+                print(f"Send `{random_number}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic {topic}")
+            sleep(self.transmissionFrequency)
 
 
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
 
 
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
-
-
-if __name__ == '__main__':
-    run()
+        
+test = Random_signal_producer("fsdfsd",1,10,1)
+test.patch()
