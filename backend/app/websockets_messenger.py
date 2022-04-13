fe87719e35a17a81ec244b3be21cd1be1069ce45
Martin Zazanjuk <martin.zazanjuk@uni-ulm.de>
2022-04-13 22:13:30 +0200

    added patch for all signals and tested with test_client

--- backend/app/websockets_messenger.py
+++ backend/app/websockets_messenger.py
@@ -1,22 +1,27 @@
-from test_client import SocketIOTestClient as socketio
+#from test_client import SocketIOTestClient as socketio
+from test_client import SocketIOTestClient
+
+from flask import Flask
+from flask_socketio import SocketIO
+
 import random, math 
 from time import sleep 
 from numpy.random import normal
 
+app = Flask(__name__)
+socketio = SocketIO(app)
 
-# TODO: find out why always 0   
-qos=0 
-def on_connect (socketio, self, app ):
-    print ("conneted", socketio)    
-def on_message(socketio, message):
-    print("recevied message" )
+test_client = SocketIOTestClient(app = app, socketio=socketio)
+test_client.connect()
 
 class Websockets_message_producer(object):
 
-    def _init_(self, type, name, args, socketio):
+    def __init__(self, type, name, args, test_client):
         #self.socketio.on_connect = on_connect
         #self.socketio.on_message = on_message
-        self.socketio = socketio
+
+        self.test_client = test_client
+        #self.socketio = socketio
         self.running = False
         self.type = type 
 
@@ -29,10 +34,7 @@ class Websockets_message_producer(object):
         elif type == 'spiked':
             self.spiked_constructor(args)
         else:
-            self.emphaiszed_constructor(args)
-
-    def on_connect (socketio, self, app ):
-        print ("conneted", socketio)  
+            self.emphaiszed_constructor(args) 
 
     def random_constructor(self,random_args):
         self.lowerBoundary = random_args["lowerBoundary"]
@@ -60,58 +62,80 @@ class Websockets_message_producer(object):
         self.center = emphasized_args["center"]
         self.scale = emphasized_args["scale"]
         self.transmissionFrequency = emphasized_args["transmissionFrequency"]
-    
+
+
+    def patch(self):
+        """[Start/Pause the signal]
+
+        Returns:
+            [Bool]: [Returns true if successful. Note there's no Return when starting a signal]
+        """
+        self.running = not self.running
+        
+        if self.running:
+            if self.type == 'random':
+                self.sendRandomSignal()
+            elif self.type =='sinus':
+                self.sendSinusSignal()
+            elif self.type == 'cosinus':
+                self.sendCosinusSignal()
+            elif self.type == 'emphasized':
+                self.sendEmphasizedRandomSignal()
+            elif self.type == 'spiked':
+                self.sendSpikedSignal()
+
+
+
     def sendRandomSignal(self):
+        """A random signal with the parameters of the corresponding object is created and sent through websocket for 'Random-Signal'.
+        """
         while(self.running):
             random_number = int(random.randint(self.lowerBoundary,self.upperBoundary))
-            socketio.emit('test_event', {'data': random_number})
+            print(f"Emitting number {random_number}")
+            socketio.emit('random_test_event', {'data': random_number})
+            print("one number emitted")
+            print(test_client.get_received())
             sleep(self.transmissionFrequency)
 
     def sendSinusSignal(self):
-        """A sinus signal with the parameters of the corresponding object is created and sent through websockets 'Sinus-Signal'.
+        """A sinus signal with the parameters of the corresponding object is created and sent through websocket for 'Sinus-Signal'.
         """
         while(self.running):
-            for i in range(0, 360) and self.running:
+            for i in range(0, 360):
                 periodic_number = self.amplitude * math.sin(self.frequency * math.radians(i))
-                result = self.socketio(self.socketio, periodic_number, qos=qos)
-                status = result[0]
-                if status == 0:
-                    print(f"Send `{periodic_number}` to socketio `{self.socketio}`")
-                else:
-                    print(f"Failed to send the message  {self.socketio}")
+                print(f"Emitting number {periodic_number}")
+                socketio.emit('sinus_test_event', {'data': periodic_number})
+                print("one number emitted")
+                print(test_client.get_received())
                 sleep(self.transmissionFrequency)
     
     def sendCosinusSignal(self):
-        """A sinus signal with the parameters of the corresponding object is created and sent through the websockets 'Sinus-Signal'.
+        """A sinus signal with the parameters of the corresponding object is created and sent through websocket for 'Sinus-Signal'.
         """
         while(self.running):
             for i in range(0, 360):
                 periodic_number = self.amplitude * math.cos(self.frequency * math.radians(i))
-                result = self.socketio(self.socketio, periodic_number, qos=qos)
-                status = result[0]
-                if status == 0:
-                    print(f"Send `{periodic_number}` to socketio `{self.socketio}`")
-                else:
-                    print(f"Failed to send message to socketio {self.socketio}")
+                print(f"Emitting number {periodic_number}")
+                socketio.emit('cosinus_test_event', {'data': periodic_number})
+                print("one number emitted")
+                print(test_client.get_received())
                 sleep(self.transmissionFrequency)
 
     def sendEmphasizedRandomSignal(self):
-        """A normally distributed signal with the parameters of the corresponding object is created and sent through the Websocktes 'Emphasized-Signal'.
+        """A normally distributed signal with the parameters of the corresponding object is created and sent through websocket for 'Emphasized-Signal'.
         """
         while(self.running):
             data = normal(loc=self.center, scale=self.scale, size=200)
             for i in data:
                 emphasized_number = i
-                result = self.socketio(self.socketio, emphasized_number, qos=qos)
-                status = result[0]
-                if status == 0:
-                    print(f"Send `{emphasized_number}` to socketio `{self.socketio}`")
-                else:
-                    print(f"Failed to send the message {self.socketio}")
+                print(f"Emitting number {emphasized_number}")
+                socketio.emit('emphasized_test_event', {'data': emphasized_number})
+                print("one number emitted")
+                print(test_client.get_received())
                 sleep(self.transmissionFrequency)
 
     def sendSpikedSignal(self):
-        """A spiked signal with the parameters of the corresponding object is created and sent to the kafka topic 'Spiked-Signal'.
+        """A spiked signal with the parameters of the corresponding object is created and sent through websocket for 'Spiked-Signal'.
         """
         i = 0
         while(self.running):
@@ -119,12 +143,10 @@ class Websockets_message_producer(object):
                 spiked_number = self.base + self.size
             else:
                 spiked_number = self.base
-            result = self.socketio(self.socketio, spiked_number, qos=qos)
-            status = result[0]
-            if status == 0:
-                print(f"Send `{spiked_number}` to topic `{self.topic}`")
-            else:
-                print(f"Failed to send the message {self.socketio}")
+            print(f"Emitting number {spiked_number}")
+            socketio.emit('spiked_test_event', {'data': spiked_number})
+            print("one number emitted")
+            print(test_client.get_received())
             sleep(self.transmissionFrequency)
             i = i + 1
 
