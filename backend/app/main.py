2a59977204e519769044fca3580507e6c9ef8c68
Florian Hauss <florian.hauss@uni-ulm.de>
2022-04-25 15:08:54 +0200

    adjusted run flask run command

--- backend/app/main.py
+++ backend/app/main.py
@@ -191,5 +191,5 @@ api.add_resource(HandleSignals,'/api/<string:publisher>/<string:signal_type>/<st
 
 
 if __name__ == "__main__":
-    #socketio.run(app = app, debug=True, host="0.0.0.0", port=5000)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    socketio.run(app = app, debug=True, host="0.0.0.0", port=5000)
+    #app.run(debug=True, host="0.0.0.0", port=5000)
