359e4e6fd681daae6faac555a5f17aa71cf8316d
Martin Zazanjuk <martin.zazanjuk@uni-ulm.de>
2022-04-27 20:30:47 +0200

    added const for better readability, cleanup

--- cypress/integration/spikedsignal.spec.js
+++ cypress/integration/spikedsignal.spec.js
@@ -1,36 +1,60 @@
 /// <reference types="cypress" />
 
+const distributedSignalName = '.css-1a0kl2o-MuiStack-root > :nth-child(1) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const distributedSignalBase = ':nth-child(2) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const distributedSignalDistance = '.css-1a0kl2o-MuiStack-root > :nth-child(3) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const distributedSignalSize = ':nth-child(4) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const distributedProbability = ':nth-child(5) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const distributedTransmissionFrequency = ':nth-child(6) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const signalRepresentation = '.css-1kav7zp-MuiPaper-root > .MuiPaper-root';
+const activeSignalsList = '.css-ne2ks3-MuiGrid-root > .MuiTypography-root';
+
 it("Create Spiked Signal and verify that one signal with name 'spikedsignal' has been created", () => {
     cy.visit("/")
     cy.get("button").contains("SPIKED SIGNAL").click()
-    cy.get('#outlined-basic').type("spikedsignal").should("have.value", "spikedsignal")
-    cy.get(':nth-child(4) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(2)
+
+    cy.get(distributedSignalName)
+    .type("spikedsignal")
+    .should("have.value", "spikedsignal")
+
+    cy.get(distributedSignalBase)
+    .type(2)
     .should('have.value', 2)
-    cy.get(':nth-child(6) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(4)
+
+    cy.get(distributedSignalDistance)
+    .type(4)
     .should('have.value', 4)
-    cy.get(':nth-child(8) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(6)
+
+    cy.get(distributedSignalSize)
+    .type(6)
     .should('have.value', 6)
-    cy.get(':nth-child(10) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(8)
+
+    cy.get(distributedProbability)
+    .type(8)
     .should('have.value', 8)
-    cy.get(':nth-child(12) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(10)
+
+    cy.get(distributedTransmissionFrequency)
+    .type(10)
     .should('have.value', 10)
+
     cy.get("[data-testid='ArrowDropDownIcon']").click()
-    cy.contains("#mui-2-option-2", 'websocket').click()
+    cy.contains("#mui-2-option-2", 'Websocket').click()
     cy.contains("button", "CREATE").click()
-   
-    cy.intercept('GET', 'http://localhost:5000/api/signals/')
+    cy.contains("button", "CREATE").click()
     
-    cy.get('.css-1kav7zp-MuiPaper-root > .MuiPaper-root').should("contain", "spikedsignal")
-    cy.get('.css-ne2ks3-MuiGrid-root > .MuiTypography-root').should("contain", "0 / 1")
+    cy.get(signalRepresentation).should("contain", "spikedsignal")
+    cy.get(activeSignalsList).should("contain", "0 / 1")
+
     cy.get('[data-testid="PlayArrowRoundedIcon"]').click()
     cy.wait(2000)
-    cy.get('.css-g7hzhr-MuiStack-root > .MuiTypography-root').should("contain", "running..")
+    
+    cy.get(signalRepresentation).should("contain", "running..")
     cy.get('[data-testid="StopCircleRoundedIcon"]').click()
     cy.get('[data-testid="StopCircleRoundedIcon"]').click()
     
-    cy.get('.css-g7hzhr-MuiStack-root > .MuiTypography-root').should("not.contain", "running..")
-    cy.get('.css-g7hzhr-MuiStack-root > .MuiTypography-root').should("contain", "stopped")
+    cy.get(signalRepresentation).should("not.contain", "running..")
+    cy.get(signalRepresentation).should("contain", "stopped")
 
     cy.get('[data-testid="DeleteForeverIcon"]').click()
-    cy.get('.css-ne2ks3-MuiGrid-root > .MuiTypography-root').should("not.contain", "0 / 1")
+    cy.get(activeSignalsList).should("not.contain", "0 / 1")
 })
