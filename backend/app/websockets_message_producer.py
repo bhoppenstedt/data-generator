8b4eef11b448b3cb63a0f30b6d605122aa833f1e
Martin Zazanjuk <martin.zazanjuk@uni-ulm.de>
2022-04-27 22:30:06 +0200

    added comments

--- backend/app/websockets_message_producer.py
+++ backend/app/websockets_message_producer.py
@@ -9,18 +9,40 @@ from time import sleep
 from numpy.random import normal
 
 
-
-
-
 class Websockets_message_producer(object):
 
+    """
+    This class handles all signals that are published through the websockets protocol.
+    For each new signal an object of this class is created and saved into the dictionaries
+    that are specified in main.py.
+    """    
+
+
     def __init__(self, type, name, args, socketio):
-    
+        """
+        This is called when a new signal that uses the mqtt message protocol is created.
+        An object that contains the necessary parameters of a signal in its variables is created.
+        It is to noted that signalstreams are not initially running on creation and need to be patched
+        once. Also note that this object is polymorphic, which means that the args 
+        dictionary contains different entries for different signal types.
+
+        Args:
+            type (String): The type of the signal. The following values are valid: 'random', 'sinus', 'cosinus', 'emphasized', 'spiked'.
+            name (String): The name of the signal.
+            args (Dictionary): A dictionary that contains all parameters of the corresponding signal.
+        """
+        # The topic to which the signal gets published to is set
         self.topic = name
+
+        # Create a SocketIO instance
         self.socketio = socketio
+
+        # Declaration of state and type of the signal
         self.running = False
         self.type = type 
 
+        # Depending on the type of the new signal a corresponding function is called that 
+        # stores the parameters into the objects variables.
         if type == 'random':
             self.random_constructor(args)
         elif type == 'sinus':
@@ -33,21 +55,71 @@ class Websockets_message_producer(object):
             self.emphaiszed_constructor(args) 
 
     def random_constructor(self,random_args):
+
+        """
+        Called when a new signal of the type 'random' is created.
+
+        Args:
+            random_args (Dictionary): A dictionary containing the necessary parameters for a random signal: 
+
+            lowerBoundary (float): The lower boundary of the random signal value
+            upperBoundary (float): The upper boundary of the random signal value
+            transmissionFrequency(float): The pause in between random signal ticks
+        """ 
+
         self.lowerBoundary = random_args["lowerBoundary"]
         self.upperBoundary = random_args["upperBoundary"]
         self.transmissionFrequency = random_args["transmissionFrequency"]
 
     def sinus_constructor(self,sinus_args):
+
+        """
+        Called when a new signal of the type 'sinus' is created.
+
+        Args:
+            sinus_args (Dictionary): A dictionary containing the necessary parameters for sinus signal: 
+
+            frequency (float): The frequency of the sinus signal
+            amplitude (float): The amplitude of the sinus signal value
+            transmissionFrequency(float): The pause in between sinus signal ticks
+        """ 
+
         self.frequency = sinus_args["frequency"]
         self.amplitude = sinus_args["amplitude"]
         self.transmissionFrequency = sinus_args["transmissionFrequency"]
 
     def cosinus_constructor(self,cosinus_args):
+
+        """
+        Called when a new signal of the type 'cosinus' is created.
+
+        Args:
+            cosinus_args (Dictionary): A dictionary containing the necessary parameters for a cosinus signal: 
+
+            frequency (float): The frequency of the cosinus signal
+            amplitude (float): The amplitude of the cosinus signal value
+            transmissionFrequency(float): The pause in between cosinus signal ticks
+        """ 
+
         self.frequency = cosinus_args["frequency"]
         self.amplitude = cosinus_args["amplitude"]
         self.transmissionFrequency = cosinus_args["transmissionFrequency"]
 
     def spiked_constructor(self,spiked_args):
+
+        """
+        Called when a new signal of the type 'spiked' is created.
+
+        Args:
+            spiked_args (Dictionary): A dictionary which contains the necessary parameters for a spiked signal which are: 
+
+            base (float): The spiked signal's baseline
+            distance (float): The amount of ticks in between potential spikes 
+            probability (float): The probability that a spike occurs (must be in a range of 0-1)
+            size (float): The size of the spikes 
+            transmissionFrequency(float): The pause in between ticks of the spiked signal
+        """ 
+
         self.base = spiked_args["base"]
         self.distance = spiked_args["distance"]
         self.propability = spiked_args["propability"]
@@ -55,16 +127,29 @@ class Websockets_message_producer(object):
         self.transmissionFrequency = spiked_args["transmissionFrequency"]
 
     def emphaiszed_constructor(self,emphasized_args):
+
+        """
+        Called when a new signal of the type 'emphasized' (normally distributed) is created.
+
+        Args:
+            emphasized_args (Dictionary): A dictionary which contains the necessary parameters for an emphasized signal which are: 
+
+            center (float): The center of the normally distributed signal 
+            scale (float): The standard deviation of the normally distributed signal
+            transmissionFrequency(float): The pause in between ticks of the emphasized signal
+        """ 
+
         self.center = emphasized_args["center"]
         self.scale = emphasized_args["scale"]
         self.transmissionFrequency = emphasized_args["transmissionFrequency"]
 
 
     def patch(self):
-        """[Start/Pause the signal]
+        """
+        [Start/Pause the signal]
 
         Returns:
-            [Bool]: [Returns true if successful. Note there's no Return when starting a signal]
+            [Bool]: [Returns true if successful. Note that there is no Return when a signal is started]
         """
         self.running = not self.running
         
@@ -83,7 +168,9 @@ class Websockets_message_producer(object):
 
 
     def sendRandomSignal(self):
-        """A random signal with the parameters of the corresponding object is created and sent through websocket for 'Random-Signal'.
+        """
+        A random signal with the parameters of the corresponding object is created
+        and sent through websocket for 'Random-Signal' with emit.
         """
         while(self.running):
             random_number = random.uniform(self.lowerBoundary,self.upperBoundary)
@@ -91,7 +178,9 @@ class Websockets_message_producer(object):
             sleep(self.transmissionFrequency)
 
     def sendSinusSignal(self):
-        """A sinus signal with the parameters of the corresponding object is created and sent through websocket for 'Sinus-Signal'.
+        """
+        A sinus signal with the parameters of the corresponding object is created
+        and sent through websocket for 'Sinus-Signal' with emit.
         """
         while(self.running):
             for i in range(0, 360):
@@ -102,7 +191,9 @@ class Websockets_message_producer(object):
                     break
     
     def sendCosinusSignal(self):
-        """A sinus signal with the parameters of the corresponding object is created and sent through websocket for 'Sinus-Signal'.
+        """
+        A sinus signal with the parameters of the corresponding object is created
+        and sent through websocket for 'Sinus-Signal' with emit.
         """
         while(self.running):
             for i in range(0, 360):
@@ -113,7 +204,9 @@ class Websockets_message_producer(object):
                     break
 
     def sendEmphasizedRandomSignal(self):
-        """A normally distributed signal with the parameters of the corresponding object is created and sent through websocket for 'Emphasized-Signal'.
+        """
+        A normally distributed signal with the parameters of the corresponding object is created
+        and sent through websocket for 'Emphasized-Signal' with emit.
         """
         while(self.running):
             data = normal(loc=self.center, scale=self.scale, size=200)
@@ -125,7 +218,9 @@ class Websockets_message_producer(object):
                     break
 
     def sendSpikedSignal(self):
-        """A spiked signal with the parameters of the corresponding object is created and sent through websocket for 'Spiked-Signal'.
+        """
+        A spiked signal with the parameters of the corresponding object is created
+        and sent through websocket for 'Spiked-Signal' with emit.
         """
         i = 0
         while(self.running):
