c58fafaa97319abd81167e456d49ae21917568ab
Martin Zazanjuk <martin.zazanjuk@uni-ulm.de>
2022-04-27 20:27:59 +0200

    added const for better readability, changed some cy.gets

--- cypress/integration/text.spec.js
+++ cypress/integration/text.spec.js
@@ -1,5 +1,17 @@
 /// <reference types="cypress" />
 
+
+const sinusSignalName = '.css-1a0kl2o-MuiStack-root > :nth-child(1) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalName = '.css-1a0kl2o-MuiStack-root > :nth-child(1) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const sinusSignalFrequency = ':nth-child(2) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalFrequency = ':nth-child(2) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const sinusSignalAmplitude = '.css-1a0kl2o-MuiStack-root > :nth-child(3) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalAmplitude = '.css-1a0kl2o-MuiStack-root > :nth-child(3) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const sinusSignalTransmissionFrequency = ':nth-child(4) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalTransmissionFrequency = ':nth-child(4) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const sinusSignalRepresentation = '.css-1kav7zp-MuiPaper-root > .MuiPaper-root';
+const activeSignalsList = '.css-ne2ks3-MuiGrid-root > .MuiTypography-root';
+
 beforeEach(() => {
     cy.visit("/")
   })
@@ -17,25 +29,37 @@ it("Verify that text exists on the website", () => {
 
 it("Verify that inputfield text gets doesn't get transferred to other signals", () => {
     cy.get("button").contains("SINUS SIGNAL").click()
-    cy.get('#outlined-basic').type("name1").should("have.value", "name1")
-    cy.get(':nth-child(4) > .MuiOutlinedInput-root > #formatted-numberformat-input')
+    cy.get(sinusSignalName)
+    .type("name1")
+    .should("have.value", "name1")
+
+    cy.get(sinusSignalFrequency)
     .type(10)
     .should("have.value", 10)
-    cy.get(':nth-child(6) > .MuiOutlinedInput-root > #formatted-numberformat-input')
+
+    cy.get(sinusSignalAmplitude)
     .type(11)
     .should("have.value", 11)
-    cy.get(':nth-child(8) > .MuiOutlinedInput-root > #formatted-numberformat-input')
+
+    cy.get(sinusSignalTransmissionFrequency)
     .type(12)
     .should("have.value", 12)
+
     cy.get("[data-testid='ArrowDropDownIcon']").click()
-    cy.contains("#mui-2-option-2", 'websocket').click()
+    cy.contains("#mui-2-option-2", 'Websocket').click()
+
     cy.get("button").contains("COSINUS SIGNAL").click()
-    cy.get('#outlined-basic').should("not.have.value", "name1")
-    cy.get(':nth-child(4) > .MuiOutlinedInput-root > #formatted-numberformat-input')
+    
+    cy.get(cosinusSignalName)
+    .should("not.have.value", "name1")
+
+    cy.get(cosinusSignalFrequency)
     .should("not.have.value", 10)
-    cy.get(':nth-child(6) > .MuiOutlinedInput-root > #formatted-numberformat-input')
+
+    cy.get(cosinusSignalAmplitude)
     .should("not.have.value", 11)
-    cy.get(':nth-child(8) > .MuiOutlinedInput-root > #formatted-numberformat-input')
+
+    cy.get(cosinusSignalTransmissionFrequency)
     .should("not.have.value", 12)
 })
 
