322b000493389511448b415f5f7dfbcffded571e
Martin Zazanjuk <martin.zazanjuk@uni-ulm.de>
2022-04-27 20:30:31 +0200

    added const for better readability, cleanup

--- cypress/integration/cosinussignal.spec.js
+++ cypress/integration/cosinussignal.spec.js
@@ -1,32 +1,49 @@
 /// <reference types="cypress" />
 
+const cosinusSignalName = '.css-1a0kl2o-MuiStack-root > :nth-child(1) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalFrequency = ':nth-child(2) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalAmplitude = '.css-1a0kl2o-MuiStack-root > :nth-child(3) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const cosinusSignalTransmissionFrequency = ':nth-child(4) > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiOutlinedInput-input';
+const signalRepresentation = '.css-1kav7zp-MuiPaper-root > .MuiPaper-root';
+const activeSignalsList = '.css-ne2ks3-MuiGrid-root > .MuiTypography-root';
+
 it("Create Cosinus Signal and verify that one signal with name 'cosinussignal' has been created", () => {
     cy.visit("/")
     cy.get("button").contains("COSINUS SIGNAL").click()
-    cy.get('#outlined-basic').type("cosinussignal").should("have.value", "cosinussignal")
-    cy.get(':nth-child(4) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(2)
-    .should('have.value', 2)
-    cy.get(':nth-child(6) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(4)
-    .should('have.value', 4)
-    cy.get(':nth-child(8) > .MuiOutlinedInput-root > #formatted-numberformat-input').type(6)
-    .should('have.value', 6)
-    cy.get("[data-testid='ArrowDropDownIcon']").click()
-    cy.contains("#mui-2-option-2", 'websocket').click()
-    cy.contains("button", "CREATE").click()
 
-    cy.intercept('GET', 'http://localhost:5000/api/signals/')
+    cy.get(cosinusSignalName)
+    .type("cosinussignal")
+    .should("have.value", "cosinussignal")
+
+    cy.get(cosinusSignalFrequency)
+    .type(2)
+    .should('have.value', 2)
+
+    cy.get(cosinusSignalAmplitude)
+    .type(4)
+    .should('have.value', 4)
+
+    cy.get(cosinusSignalTransmissionFrequency)
+    .type(6)
+    .should('have.value', 6)
+
+    cy.get("[data-testid='ArrowDropDownIcon']").click()
+    cy.contains("#mui-2-option-2", 'Websocket').click()
+    cy.contains("button", "CREATE").click()
+    cy.contains("button", "CREATE").click()
     
-    cy.get('.css-1kav7zp-MuiPaper-root > .MuiPaper-root').should("contain", "cosinussignal")
-    cy.get('.css-ne2ks3-MuiGrid-root > .MuiTypography-root').should("contain", "0 / 1")
+    cy.get(signalRepresentation).should("contain", "cosinussignal")
+    cy.get(activeSignalsList).should("contain", "0 / 1")
     cy.get('[data-testid="PlayArrowRoundedIcon"]').click()
+    
     cy.wait(2000)
-    cy.get('.css-g7hzhr-MuiStack-root > .MuiTypography-root').should("contain", "running..")
+    cy.get(signalRepresentation).should("contain", "running..")
     cy.get('[data-testid="StopCircleRoundedIcon"]').click()
     cy.get('[data-testid="StopCircleRoundedIcon"]').click()
     
-    cy.get('.css-g7hzhr-MuiStack-root > .MuiTypography-root').should("not.contain", "running..")
-    cy.get('.css-g7hzhr-MuiStack-root > .MuiTypography-root').should("contain", "stopped")
+    cy.get(signalRepresentation).should("not.contain", "running..")
+    cy.get(signalRepresentation).should("contain", "stopped")
 
     cy.get('[data-testid="DeleteForeverIcon"]').click()
-    cy.get('.css-ne2ks3-MuiGrid-root > .MuiTypography-root').should("not.contain", "0 / 1")
+    cy.get(activeSignalsList).should("not.contain", "0 / 1")
 })
